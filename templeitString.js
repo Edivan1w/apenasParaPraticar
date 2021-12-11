e25f0bd4ad2ecb8695615193eb39a9aa83cb3c4c
EDIVAN <edwanestudos@gmail.com>
2021-12-11 18:57:29 -0300

    Aprendendo sobre variáveis.

---
+++ templeitString.js
@@ -0,0 +1,8 @@
+const nome = "Edivan";
+const idade = 29;
+const cidade = "Cacoal";
+const texto = ``;
+const frase = `Meu nome é ${nome}, minha idade é ${idade} anos e moro em ${cidade}.`;
+
+console.log(frase);
+console.log(texto);
---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeCliente = 10;
+const idadePermicao = 18;
+
+if(idadeCliente >= idadePermicao) {
+    console.log("cerveja")
+}else{
+    console.log("suco")
+}
+// ternario
+console.log(idadeCliente >= idadePermicao ? "cerveja" : "suco")
